4dcebd9f67f85699e2f3cce8d69af0e14a1c0737
Angelo Viero <angelosviero@gmail.com>
2025-05-15 15:06:28 -0300

    refactor(routes): organização e padronização das rotas

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,31 +1,42 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\NewsController;
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\Admin\AdminNewsController;
 use App\Http\Controllers\Admin\AdminUserController;
-use App\Http\Controllers\AdminController;
-use App\Http\Controllers\NewsController;
-use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('home');
-});
 
+// Página inicial
 Route::get('/', [NewsController::class, 'index'])->name('home');
 
-Route::get('/noticias', [NewsController::class, 'all'])->name('news.index');
+// Rota de busca
+Route::get('/noticias/busca', [NewsController::class, 'search'])->name('news.search');
 
-Route::get('/noticias/{news}', [NewsController::class, 'show'])->name('news.show');
+// Notícias
+Route::prefix('noticias')->name('news.')->group(function () {
+    Route::get('/', [NewsController::class, 'all'])->name('index');
+    Route::get('/{news}', [NewsController::class, 'show'])->name('show');
+});
 
-// Rota de admin
-// Grupo protegido por autenticação e middleware 'auth'
+// Rotas administrativas
 Route::middleware(['auth'])->prefix('admin')->name('admin.')->group(function () {
+    // Dashboard Administrativo
     Route::get('/', [AdminController::class, 'index'])->name('dashboard');
 
-    // Gerenciamento de Notícias
+    // CRUD de Notícias
     Route::resource('noticias', AdminNewsController::class);
 
-    // Gerenciamento de Usuários (opcional - mostrar, editar e deletar usuários)
+    // Gerenciamento básico de usuários
     Route::resource('usuarios', AdminUserController::class)->only(['index', 'edit', 'update', 'destroy']);
 });
 
-require __DIR__.'/auth.php';
+// Botões Header
+Route::view('/institucional', 'institucional')->name('institucional');
+Route::View('/segurados', 'segurados')->name('segurados');
+Route::View('/dependentes', 'dependentes')->name('dependendetes');
+Route::View('/servidores', 'servidores')->name('servidores');
+Route::View('/legislacao', 'legislacao')->name('legislacao');
+
+// Autenticação (Laravel Breeze, Fortify, etc.)
+require __DIR__ . '/auth.php';
